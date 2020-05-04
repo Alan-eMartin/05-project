480c4e91af73202cbc166de81d19a062184a3acd
Alan <alan.edward.martin@gmail.com>
2020-05-03 23:51:27 -0400

    Updates

---
+++ precache-manifest.eb579e479ffb5fd3a64fec31b71cd231.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8014ade81d7891146323dd559d82718",
+    "url": "/05-project/index.html"
+  },
+  {
+    "revision": "b81c5940e0cd9397099d",
+    "url": "/05-project/static/css/main.728e2107.chunk.css"
+  },
+  {
+    "revision": "a2bb11a3ef8ec7f5158f",
+    "url": "/05-project/static/js/2.a0744837.chunk.js"
+  },
+  {
+    "revision": "4ae2e24854201e2c90f766e681fee893",
+    "url": "/05-project/static/js/2.a0744837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b81c5940e0cd9397099d",
+    "url": "/05-project/static/js/main.324058a7.chunk.js"
+  },
+  {
+    "revision": "9e65cad9ddb3eca185ff",
+    "url": "/05-project/static/js/runtime-main.189f07fb.js"
+  },
+  {
+    "revision": "43f8d0f776a88e54d114475572f6d7dd",
+    "url": "/05-project/static/media/bgImg.43f8d0f7.JPG"
+  },
+  {
+    "revision": "7eb7bd4dfc25d42c117b48f89f7d7fe4",
+    "url": "/05-project/static/media/bioImg.7eb7bd4d.jpg"
+  }
+]);
